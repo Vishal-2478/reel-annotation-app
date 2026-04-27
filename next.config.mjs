3f90af56d1ddf177c0d98580c1c7827223cb3d0a
Vishal <vishalq2478@gmail.com>
2026-04-27 23:14:46 +0530

    fixed pwa turbopack config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,12 @@ import nextPWA from "next-pwa";
 
 const withPWA = nextPWA({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  turbopack: {}
+};
 
 export default withPWA(nextConfig);
