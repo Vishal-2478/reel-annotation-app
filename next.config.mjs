131a8b1e770d93ed6d7658e86f0c8732ceb4d391
Vishal <vishalq2478@gmail.com>
2026-04-27 23:06:11 +0530

    added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
+import nextPWA from "next-pwa";
 
-export default nextConfig;
+const withPWA = nextPWA({
+  dest: "public",
+});
+
+const nextConfig = {};
+
+export default withPWA(nextConfig);
